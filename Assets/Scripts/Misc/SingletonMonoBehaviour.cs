87ddff9429dbe4e71586763616f7867f97791d3f
Drinnn <pedro_lontro@hotmail.com>
2020-09-24 11:05:24 -0300

    feat: add singleton generic class and initial player class

---
+++ Assets/Scripts/Misc/SingletonMonoBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public abstract class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour {
+    public static T Instance {
+        get {
+            return _instance;
+        }
+    }
+
+    private static T _instance;
+
+    protected virtual void Awake() {
+        if (_instance == null)
+            _instance = this as T;
+        else
+            Destroy(gameObject);
+    }
+
+}
